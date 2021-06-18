71a278e2cac17d245f5264868a01e64263f71008
Peter Foris <peterforis6@gmail.com>
2021-06-18 18:02:07 +0200

    Move gatewayAddress to application.properties

--- src/main/java/com/example/zeebedemo/ZeebeClientFactory.java
+++ src/main/java/com/example/zeebedemo/ZeebeClientFactory.java
@@ -3,15 +3,18 @@ package com.example.zeebedemo;
 import io.camunda.zeebe.client.ZeebeClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:application.properties")
 public class ZeebeClientFactory {
     private Logger LOG = LoggerFactory.getLogger(this.getClass());
 
-    //	TODO: @Value
-    private String gatewayAddress = "127.0.0.1:26500";
+    @Value("${gatewayAddress}")
+    private String gatewayAddress;
 
     @Bean
     public ZeebeClient zeebeClient(){
