9d40f2a3650b860b60605f556606a37a216ebe89
Peter Foris <peterforis6@gmail.com>
2021-06-18 16:35:12 +0200

    Zeebe client setup

--- src/main/java/com/example/zeebedemo/ZeebeDemoApplication.java
+++ src/main/java/com/example/zeebedemo/ZeebeDemoApplication.java
@@ -10,47 +10,58 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 public class ZeebeDemoApplication {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ZeebeDemoApplication.class);
+	private Logger LOG = LoggerFactory.getLogger(ZeebeDemoApplication.class);
+
+	private ZeebeClient client;
+
+//	TODO: @Value
+	private String gatewayAddress = "127.0.0.1:26500";
 
     public static void main(String[] args) {
         SpringApplication.run(ZeebeDemoApplication.class, args);
     }
 
+    @PostConstruct
+    public void setup() {
+        client = ZeebeClient.newClientBuilder()
+                .gatewayAddress(gatewayAddress)
+                .usePlaintext()
+                .build();
+        LOG.info("Zeebe client connected to {}", gatewayAddress);
+    }
+
     @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		doZeebe();
-    	return String.format("Hello %s!", name);
+    	return String.format("Hello %s!\n", name);
     }
 
     public void doZeebe() {
-        final ZeebeClient client = ZeebeClient.newClientBuilder()
-                .gatewayAddress("127.0.0.1:26500")
-                .usePlaintext()
-                .build();
-        LOG.info("Connected.");
 
         final DeploymentEvent deployment = client.newDeployCommand()
-                .addResourceFromClasspath("order-process.bpmn")
+                .addResourceFromClasspath("hello-process.bpmn")
                 .send()
                 .join();
 
         final int version = deployment.getProcesses().get(0).getVersion();
         LOG.info("Workflow deployed. Version: " + version);
 
-        final WorkflowInstanceEvent wfInstance = client.newCreateInstanceCommand()
-                .bpmnProcessId("order-process")
+        final ProcessInstanceEvent processInstanceEvent = client.newCreateInstanceCommand()
+                .bpmnProcessId("HelloProcess")
                 .latestVersion()
                 .send()
                 .join();
 
-        final long workflowInstanceKey = wfInstance.getWorkflowInstanceKey();
-
-        System.out.println("Workflow instance created. Key: " + workflowInstanceKey);
-
+        final long processInstanceKey = processInstanceEvent.getProcessInstanceKey();
+        LOG.info("Workflow instance created. Key: " + processInstanceKey);
 
         client.close();
         LOG.info("Closed.");
